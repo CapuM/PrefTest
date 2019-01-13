34103592bc7ceb9cf63fc0a075dfc8d376d3d765
CapuM <bluevelvet1896@gmail.com>
2019-01-13 11:22:37 +0100

    wow

--- app/src/main/java/com/example/matteo/preftest/WelcomeFragment.java
+++ app/src/main/java/com/example/matteo/preftest/WelcomeFragment.java
@@ -2,6 +2,7 @@ package com.example.matteo.preftest;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,12 +36,15 @@ public class WelcomeFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_welcome, container, false);
         container.removeAllViews();
+
+
+
 
         checkList=view.findViewById(R.id.checkable_list);
         checkList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
